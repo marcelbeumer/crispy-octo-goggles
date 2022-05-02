d001c0e6b3f06a24df9463670fbe9e66fd478d1c
Marcel Beumer <marcel@marcelbeumer.com>
2022-05-03 01:19:42 +0200

    copy over code from websocket

--- chatbox/main.go
+++ chatbox/main.go
@@ -1,9 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alecthomas/kong"
+	"github.com/marcelbeumer/crispy-octo-goggles/chatbox/client"
+	"github.com/marcelbeumer/crispy-octo-goggles/chatbox/server"
+)
+
+type ClientServerOpts struct {
+	Host string `help:"Server host." short:"h" default:"127.0.0.1"`
+	Port int    `help:"Server port." short:"p" default:"9998"`
+}
+
+type Commands struct {
+	Client struct {
+		ClientServerOpts
+	} `cmd:"client" help:"Start client"`
+	Server struct {
+		ClientServerOpts
+	} `cmd:"client" help:"Start server"`
+}
 
 func main() {
-	fmt.Println("new")
-
-	// cmd.Execute()
+	cli := Commands{}
+	ctx := kong.Parse(
+		&cli,
+		kong.Name("chatbox"),
+		kong.UsageOnError(),
+	)
+	switch ctx.Command() {
+	case "client":
+		addr := fmt.Sprintf("%s:%d", cli.Server.Host, cli.Server.Port)
+		if err := client.StartClient(addr); err != nil {
+			panic(err)
+		}
+	case "server":
+		addr := fmt.Sprintf("%s:%d", cli.Server.Host, cli.Server.Port)
+		if err := server.StartServer(addr); err != nil {
+			panic(err)
+		}
+	}
 }
