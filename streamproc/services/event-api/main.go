9a2aefb2c35c2c65a07a2e2ca34010e8f5a76f66
Marcel Beumer <marcel@marcelbeumer.com>
2022-06-08 16:22:37 +0200

    write messages to kafka

--- streamproc/services/event-api/main.go
+++ streamproc/services/event-api/main.go
@@ -9,12 +9,106 @@ import (
 
 	"github.com/alecthomas/kong"
 	"github.com/marcelbeumer/go-playground/streamproc/services/event-api/internal/log"
-	"github.com/marcelbeumer/go-playground/streamproc/services/event-api/internal/server"
+
+	"encoding/json"
+	"math/big"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/segmentio/kafka-go"
 )
 
 type CLI struct {
-	Host string `help:"API host." short:"h" default:"127.0.0.1" env:"HOST"`
-	Port int    `help:"API port." short:"p" default:"9998"      env:"PORT"`
+	Host       string `help:"API host."    short:"h" default:"127.0.0.1" env:"HOST"`
+	Port       int    `help:"API port."    short:"p" default:"9998"      env:"PORT"`
+	KafkaHost  string `help:"Kafka host."            default:"127.0.0.1" env:"KAFKA_HOST"`
+	KafkaPort  int    `help:"Kafka port."            default:"9092"      env:"KAFKA_PORT"`
+	KafkaTopic string `help:"Kafka topic."           default:"events"    env:"KAFKA_TOPIC"`
+}
+
+type Event struct {
+	Time   int64     `json:"time"`
+	Amount big.Float `json:"amount"`
+}
+
+type PostEventsBody []Event
+
+type ResponseBody struct {
+	Message string `json:"message"`
+}
+
+type Server struct {
+	logger    log.Logger
+	kafkaConn *kafka.Conn
+}
+
+func NewServer(logger log.Logger) *Server {
+	return &Server{
+		logger: logger,
+	}
+}
+
+func (s *Server) ListenAndServe(addr string, kafkaAddr string, kafkaTopic string) error {
+	logger := s.logger
+	logger.Infow("starting server", "addr", addr)
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", kafkaAddr, kafkaTopic, 0)
+	if err != nil {
+		return err
+	}
+	s.kafkaConn = conn
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", s.postEvents).Methods("POST")
+
+	return http.ListenAndServe(addr, r)
+}
+
+func (s *Server) Shutdown(ctx context.Context) {
+	//
+}
+
+func (s *Server) writeBadRequest(err error, w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(ResponseBody{
+		Message: err.Error(),
+	})
+}
+
+func (s *Server) postEvents(w http.ResponseWriter, r *http.Request) {
+	logger := s.logger
+	var postEventsBody PostEventsBody
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(&postEventsBody)
+	if err != nil {
+		s.writeBadRequest(err, w, r)
+		return
+	}
+
+	messages := make([]kafka.Message, len(postEventsBody))
+	for i, event := range postEventsBody {
+		msg, err := json.Marshal(event)
+		if err != nil {
+			s.writeBadRequest(err, w, r)
+			return
+		}
+		messages[i] = kafka.Message{Value: msg}
+	}
+
+	s.kafkaConn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = s.kafkaConn.WriteMessages(messages...)
+	if err != nil {
+		s.writeBadRequest(err, w, r)
+		return
+	}
+
+	logger.Infow("ingested events", "count", len(postEventsBody))
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(ResponseBody{
+		Message: fmt.Sprintf("ingested %d events", len(postEventsBody)),
+	})
+	return
 }
 
 func main() {
@@ -29,10 +123,11 @@ func main() {
 	log.RedirectStdLog(zl)
 	logger := log.NewZapLoggerAdapter(zl)
 
-	srv := server.NewServer(logger)
+	srv := NewServer(logger)
 	go func() {
 		addr := fmt.Sprintf("%s:%d", cli.Host, cli.Port)
-		if err := srv.ListenAndServe(addr); err != nil {
+		kafkaAddr := fmt.Sprintf("%s:%d", cli.KafkaHost, cli.KafkaPort)
+		if err := srv.ListenAndServe(addr, kafkaAddr, cli.KafkaTopic); err != nil {
 			logger.Errorw("could not start server", log.Error(err))
 			os.Exit(1)
 		}
