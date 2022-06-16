3e9467e14fe3723e773fa4f4a1003153e665b715
Marcel Beumer <marcel@marcelbeumer.com>
2022-06-16 22:21:02 +0200

    static chart

--- streamproc/services/web-ui/script.js
+++ streamproc/services/web-ui/script.js
@@ -1,51 +1,68 @@
-let x = {
-  type: "line",
-  data: {
-    datasets: [
-      {
-        data: [
-          { x: "2016-12-25", y: 20 },
-          { x: "2016-12-26", y: 10 },
-        ],
-      },
-    ],
-  },
+let serverData = {
+  message: "ok",
+  points: [
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 0, type: "low" },
+    { time: 1655403669818, count: 65, type: "low" },
+    { time: 1655403669818, count: 159, type: "low" },
+    { time: 1655403669818, count: 29, type: "low" },
+    { time: 1655406600000, count: 70, type: "high" },
+    { time: 1655406900000, count: 146, type: "high" },
+    { time: 1655407200000, count: 37, type: "high" },
+  ],
 };
+
+let dataHigh = [];
+let dataLow = [];
+for (const p of serverData.points) {
+  var o = { x: p.time, y: p.count };
+  if (p.type === "low") {
+    dataLow.push(o);
+  } else {
+    dataHigh.push(o);
+  }
+}
+
+let colors = ["#ED6D85", "#57A0E5", "#6DBDBF"];
 
 let el = document.getElementById("chart");
 let ctx = el.getContext("2d");
 let myChart = new Chart(ctx, {
-  type: "bar",
+  type: "line",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
+        label: "Low",
+        backgroundColor: colors[0],
+        borderColor: colors[0],
+        cubicInterpolationMode: "monotone",
+        tension: 0.4,
+        data: dataLow,
+      },
+      {
+        label: "High",
+        backgroundColor: colors[1],
+        borderColor: colors[1],
+        cubicInterpolationMode: "monotone",
+        tension: 0.4,
+        data: dataHigh,
       },
     ],
   },
   options: {
     scales: {
-      y: {
-        beginAtZero: true,
+      x: {
+        type: "time",
+        min: 1655403669818 - 1000 * 60,
+        max: 1655407200000 + 1000 * 60,
       },
     },
   },
