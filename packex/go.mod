fda8e465b5ae1ca6ac699582041714302defdc5f
Marcel Beumer <marcel@marcelbeumer.com>
2022-07-07 15:38:32 +0200

    cli

--- packex/go.mod
+++ packex/go.mod
@@ -1,3 +1,5 @@
 module github.com/marcelbeumer/go-playground/packex
 
 go 1.18
+
+require github.com/alecthomas/kong v0.6.1 // indirect
--- packex/main.go
+++ packex/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alecthomas/kong"
+)
+
+type Opts struct {
+	Show struct{} `help:"Show package info"       cmd:"show"`
+	List struct{} `help:"List available packages" cmd:"list"`
+}
 
 func main() {
-	fmt.Println("Hello packex")
+	opts := Opts{}
+	ctx := kong.Parse(
+		&opts,
+		kong.Name("packex"),
+		kong.UsageOnError(),
+	)
+	switch ctx.Command() {
+	case "show":
+		fmt.Println("show")
+	case "list":
+		fmt.Println("list")
+	}
 }
