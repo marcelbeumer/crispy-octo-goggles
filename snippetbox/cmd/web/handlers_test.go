b187555681c2c5acb5e72ff6de18f1c2f8821e58
Marcel Beumer <marcel@marcelbeumer.com>
2022-07-27 16:48:49 +0200

    "Let's Go" chapter 14.1 - 14.4

---
+++ snippetbox/cmd/web/handlers_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/marcelbeumer/go-playground/snippetbox/internal/assert"
+)
+
+func TestPingE2E(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.get(t, "/ping")
+
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
+}
+
+func TestPingHandler(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ping(rr, r)
+	rs := rr.Result()
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bytes.TrimSpace(body)
+	assert.Equal(t, string(body), "OK")
+}
