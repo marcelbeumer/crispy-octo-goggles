16f9a4859893b5e20a3481c7f229e12b7895c472
Marcel Beumer <marcel@marcelbeumer.com>
2022-12-30 22:08:40 +0100

    WIP

--- ratelimiter/server.go
+++ ratelimiter/server.go
@@ -6,6 +6,10 @@ import (
 )
 
 func StartServer(addr string) error {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world!")
+	})
+
 	fmt.Printf("Starting server on %s\n", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		return err
